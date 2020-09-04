8473885acb8707174bd2cf8e7fdd45f2ce9da0a1
Paul Gaudin <paulgaudin314@gmail.com>
2020-09-04 14:56:44 -0700

    hmmph

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
-# import os
-# import xml.etree.ElementTree as ET
-# import gzip
+import os
+import xml.etree.ElementTree as ET
+import gzip
 
 
 def load_data(input_file):
@@ -9,148 +9,172 @@ def load_data(input_file):
     ## keep track of tags, whereby these change to true the next item in the appropriate element
     ## will be the relevant value 
 
-    # change chunk size based on files. usually use 1M for full UniChem data files
-    current_chunk_size = 1000000;
-    # load source files
-    source_file = os.path.join(data_folder,"UC_SOURCE.txt")
-    struct_file = os.path.join(data_folder,"UC_STRUCTURE.txt")
-    xref_file = os.path.join(data_folder,"UC_XREF.txt")
-    assert os.path.exists(source_file)
-    assert os.path.exists(struct_file)
-    assert os.path.exists(xref_file)
+    PC_count = False
+    inchi = False
+    inchikey = False  
+    hydrogen_bond_acceptor = False
+    hydrogen_bond_donor = False
+    rotatable_bond = False
+    iupac = False
+    logp = False
+    mass = False
+    molecular_formula = False
+    molecular_weight = False
+    smiles = False
+    topological = False
+    monoisotopic_weight = False
+    complexity = False
 
-    # create source dictionary, {source id: name of source}
-    source_tsv = pd.read_csv(source_file, sep='\t', header= None)
-    source_keys = list(source_tsv[0])
-    source_values = list(source_tsv[1])
-    source_dict = {source_keys[i]: source_values[i] for i in range(len(source_keys))}     
+    ## use gzip to read the .gz file
+    unzipped_file = gzip.open(input_file, 'rb')
 
+    ## use ET.iterparse to loop through the xml line by line
+    for event, elem in ET.iterparse(unzipped_file, events=("start","end")):
+        prefix, has_namespace, postfix = elem.tag.partition('}')
+        if has_namespace:
+            elem.tag = postfix  # strip all namespaces
+        ## all of the compound properties will be inside this element
+        if((elem.tag == "PC-CompoundType_id_cid") & (event == 'start')):     
+            current_compound = {}
+            compound_data = {}
+            current_compound["_id"] = elem.text
+            compound_data["cid"] = elem.text
+            compound_data["iupac"] = {}
+            compound_data["smiles"] = {}
+        elif((elem.tag == "PC-Compound") & (event == 'end')):
+            current_compound["pubchem"] = compound_data
+            ## rarely, some will be missing a cid. make sure this isn't the case
+            if(current_compound["_id"]):
+                ## yield the compound
+                yield(current_compound)
+            ## clear element from memory
+            elem.clear()
+        elif((elem.tag == "PC-Compound_charge") & (event == 'start')):
+            if(elem.text):
+                compound_data["formal_charge"] = elem.text
+        elif((elem.tag == "PC-Count") & (event == 'start')):
+            PC_count = True
+        elif((elem.tag == "PC-Count") & (event == 'end')):
+            PC_count = False
+        elif(PC_count):
+            if(elem.text):
+                if((elem.tag == "PC-Count_heavy-atom") & (event == 'start')):
+                    compound_data["heavy_atom_count"] = elem.text
+                elif((elem.tag == "PC-Count_atom-chiral-def") & (event == 'start')):
+                    compound_data["defined_chiral_atom_count"] = elem.text
+                elif((elem.tag == "PC-Count_bond-chiral-def") & (event == 'start')):
+                    compound_data["defined_chiral_bond_count"] = elem.text
+                elif((elem.tag == "PC-Count_atom-chiral-undef") & (event == 'start')):
+                    compound_data["undefined_chiral_atom_count"] = elem.text
+                elif((elem.tag == "PC-Count_bond-chiral-undef") & (event == 'start')):
+                    compound_data["undefined_chiral_bond_count"] = elem.text
+                elif((elem.tag == "PC-Count_isotope-atom") & (event == 'start')):
+                    compound_data["isotope_atom_count"] = elem.text
+                elif((elem.tag == "PC-Count_covalent-unit") & (event == 'start')):
+                    compound_data["covalent_unit_count"] = elem.text
+                elif((elem.tag == "PC-Count_tautomers") & (event == 'start')):
+                    compound_data["tautomers_count"] = elem.text
+        elif((elem.tag == "PC-Count") & (event == 'start')):
+            PC_count = True
+        elif((elem.tag == "PC-Count") & (event == 'end')):
+            PC_count = False
+            
+        elif((elem.tag == "PC-Urn_label") & (event == 'start')):
+            if(elem.text == 'InChI'):
+                inchi = True
+            elif(elem.text == 'InChIKey'):
+                inchikey = True
+            elif(elem.text == 'IUPAC Name'):
+                iupac = True
+            elif(elem.text == "Log P"):
+                logp = True
+            elif(elem.text == "Mass"):
+                mass = True
+            elif(elem.text == "Molecular Formula"):
+                molecular_formula = True
+            elif(elem.text == "Molecular Weight"):
+                molecular_weight = True
+            elif(elem.text == "SMILES"):
+                smiles = True
+            elif(elem.text == "Topological"):
+                topological = True
+            elif(elem.text == "Weight"):
+                monoisotopic_weight = True
+            elif(elem.text == "Compound Complexity"):
+                complexity = True
+
+        elif((elem.tag == "PC-Urn_name") & (event == 'start')):
+            if(elem.text == 'Hydrogen Bond Acceptor'):
+                hydrogen_bond_acceptor = True
+            elif(elem.text == 'Hydrogen Bond Donor'):
+                hydrogen_bond_donor = True
+            elif(elem.text == 'Rotatable Bond'):
+                rotatable_bond = True
+            elif(iupac):
+                iupac_key = elem.text  
+            elif(smiles):
+                smiles_key = elem.text  
+
+        elif((elem.tag == "PC-InfoData_value_sval") & (event == 'start')):
+            if(inchi):
+                if(elem.text):
+                    compound_data["inchi"] = elem.text
+                inchi = False  
+            elif(inchikey):
+                if(elem.text):
+                    compound_data["inchikey"] = elem.text
+                inchikey = False  
+            elif(iupac):
+                if(iupac_key):
+                    if(elem.text):
+                        compound_data['iupac'][iupac_key] = elem.text
+                iupac = False
+            elif(molecular_formula):
+                if(elem.text):
+                    compound_data["molecular_formula"] = elem.text
+                molecular_formula = False
+            elif(smiles):
+                if(smiles_key):
+                    if(elem.text):
+                        compound_data['smiles'][smiles_key] = elem.text
+                smiles = False
     
-    ## make structure file (condensed, then sorted) by reading and appending new file in chunks - too big to load all at once 
-    sdtype={'uci':'int64','standardinchikey':'str'}
-    
-    structure_df_chunk = pd.read_csv(struct_file, sep='\t', header=None, usecols=['uci', 'standardinchikey'],
-                                         names=['uci_old','standardinchi','standardinchikey','created','username','fikhb','uci','parent_smiles'],
-                                         chunksize=current_chunk_size, dtype=sdtype) 
-
-
-    smerge_counter = 0; # use merge counter to append file after file is created
-    for chunk in structure_df_chunk:
-        if(smerge_counter == 0):
-            chunk.to_csv(path_or_buf=os.path.join(data_folder,"structure_df.csv"), index=False)
-            smerge_counter = 1;  
-        else:
-            chunk.to_csv(path_or_buf=os.path.join(data_folder,"structure_df.csv"), index=False, mode='a', header=False)    
-
-    del structure_df_chunk # clear from memory
-
-    ## use customized csvsort function - from edited csvsort module - see csvsort folder - sort by uci (column index 1)
-    csvsort(os.path.join(data_folder,"structure_df.csv"),[1],numeric_column=True)
-
-
-    ## make xref file - condensed   
-    xdtype={'src_id':'int8','src_compound_id':'str','uci':'int64'}
-    
-    xref_df_chunk = pd.read_csv(xref_file, sep='\t', header=None, usecols=['src_id','src_compound_id', 'uci'],
-                                         names=['uci_old','src_id','src_compound_id','assignment','last_release_u_when_current','created ','lastupdated','userstamp','aux_src','uci'],
-                                         chunksize=current_chunk_size, dtype=xdtype)  
-
-    xmerge_counter = 0;
-
-    for chunk in xref_df_chunk:
-        if(xmerge_counter == 0):
-            chunk.to_csv(path_or_buf=os.path.join(data_folder,"xref_df.csv"), index=False)
-            xmerge_counter = 1;  
-        else:
-            chunk.to_csv(path_or_buf=os.path.join(data_folder,"xref_df.csv"), index=False, mode='a', header=False)    
-
-    del xref_df_chunk
-
-    ## use customized csvsort function to sort xref file by uci (column index 2)
-    csvsort(os.path.join(data_folder,"xref_df.csv"),[2],numeric_column=True)
-
-
-    ## make data frame that keeps track of min and max uci value for each structure chunk 
-    chunk_counter = 0;
-    structure_df_chunk = pd.read_csv(os.path.join(data_folder,"structure_df.csv"), chunksize=current_chunk_size)
-    min_max_columns = ["chunk_start", "min_uci", "max_uci"];
-    structure_min_max_df = pd.DataFrame(columns = min_max_columns)
-    for schunk in structure_df_chunk:
-        chunk_start = chunk_counter*current_chunk_size
-        chunk_min = min(schunk["uci"])
-        chunk_max = max(schunk["uci"])
-        chunk_counter = chunk_counter + 1;
-        structure_min_max_df = structure_min_max_df.append(pd.DataFrame([[chunk_start,chunk_min,chunk_max]], columns = min_max_columns))
-    
-    xdf_chunk = pd.read_csv(os.path.join(data_folder,"xref_df.csv"), chunksize=current_chunk_size) 
-
-    ## loop through xdf chunks. merge with all structure chunks that have overlapping uci values
-    merge_counter = 0; 
-    for xchunk in xdf_chunk:
-        current_x_min = min(xchunk["uci"])
-        current_x_max = max(xchunk["uci"])
-        for index, row in structure_min_max_df.iterrows():
-            if(not((current_x_max < row['min_uci']) or (current_x_min > row['max_uci']))):
-                sdf_chunk = pd.read_csv(os.path.join(data_folder,"structure_df.csv"), skiprows = row["chunk_start"], header=0, names=['standardinchikey','uci'], nrows=current_chunk_size)
-                complete_df_chunk = pd.merge(left=sdf_chunk, right=xchunk, left_on='uci', right_on='uci')
-                if(merge_counter == 0):
-                    complete_df_chunk.to_csv(path_or_buf=os.path.join(data_folder,"complete_df.csv"), index=False)
-                    merge_counter = 1;  
-                else:
-                    complete_df_chunk.to_csv(path_or_buf=os.path.join(data_folder,"complete_df.csv"), index=False, mode='a', header=False)
-        
-        
-    del sdf_chunk
-    del xdf_chunk 
-
-    ## sort complete_df (merged structure and xref file) based on inchikey - alphabetically     
-    csvsort(os.path.join(data_folder,"complete_df.csv"),[0], numeric_column = False)
-
-
-
-    ## loop through merged complete data frame in chunks
-    complete_df_chunk = pd.read_csv(os.path.join(data_folder,"complete_df.csv"), chunksize=current_chunk_size)
-
-
-    new_entry = {} ## each entry will be made based on inchikey
-    last_inchi = ''; ## keep track of the inchikey from the previous row looked at in the complete dataframe
-
-    for chunk in complete_df_chunk:
-        for row in chunk.itertuples(): 
-            inchi = row[1]
-            source = source_dict[row[3]]
-            source_id = row[4]
-            # make sure there are no missing values in entry (would show as nan)
-            if((source_id == source_id) and (source == source) and (inchi == inchi)):
-                # reformat chebi source id to fit MyChem.info syntax 
-                if(source == 'chebi'):
-                    source_id = 'CHEBI:' + source_id
-                # check to see if previous entry had same inchi code. if so, 
-                if(last_inchi == inchi):
-                    # if source id already exists for source, then create/add to list. if not, create first entry for source
-                    if(source in new_entry["unichem"]):
-                        if(type(new_entry["unichem"][source]) == str):
-                            new_entry["unichem"][source] = [new_entry["unichem"][source], source_id] 
-                        else:
-                            new_entry["unichem"][source].append(source_id) 
-                    else:
-                        new_entry["unichem"][source] = source_id
-                elif(len(last_inchi) == 0): 
-                    new_entry = {
-                        "_id" : inchi,
-                        "unichem": {
-                            source: source_id
-                        }
-                    }
-                    last_inchi = inchi
-                else:
-                    yield new_entry; ## yield created entry from previous row(s) when inchikey changes
-                    new_entry = {
-                        "_id" : inchi,
-                        "unichem": {
-                            source: source_id
-                        }
-                    }
-                last_inchi = inchi ## set last_inchi to the inchikey used in current iteration 
-
-    yield new_entry ## submit final entry 
+        elif((elem.tag == "PC-InfoData_value_ival") & (event == 'start')):
+            if(hydrogen_bond_acceptor):
+                if(elem.text):
+                    compound_data["hydrogen_bond_acceptor_count"] = elem.text
+                hydrogen_bond_acceptor = False
+            elif(hydrogen_bond_donor):
+                if(elem.text):
+                    compound_data["hydrogen_bond_donor_count"] = elem.text
+                hydrogen_bond_donor = False
+            elif(rotatable_bond):
+                if(elem.text):
+                    compound_data["rotatable_bond_count"] = elem.text
+                rotatable_bond = False
+            
+        elif((elem.tag == "PC-InfoData_value_fval") & (event == 'start')):
+            if(logp):
+                if(elem.text):
+                    compound_data["xlogp"] = elem.text
+                logp = False
+            elif(mass):
+                if(elem.text):
+                    compound_data["exact_mass"] = elem.text
+                mass = False
+            elif(molecular_weight): 
+                if(elem.text):
+                    compound_data["molecular_weight"] = elem.text
+                molecular_weight = False
+            elif(topological):
+                if(elem.text):
+                    compound_data["topological_polar_surface_area"] = elem.text
+                topological = False
+            elif(monoisotopic_weight):
+                if(elem.text):
+                    compound_data["monoisotopic_weight"] = elem.text
+                monoisotopic_weight = False
+            elif(complexity):
+                if(elem.text):
+                    compound_data["complexity"] = elem.text
+                complexity = False
